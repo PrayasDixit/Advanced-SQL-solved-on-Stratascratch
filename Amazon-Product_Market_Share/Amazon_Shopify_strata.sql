cf1fa65e8daa22c62da396f73b98c6a7f1fb6d8b
Prayas Dixit <98242623+PrayasDixit@users.noreply.github.com>
2024-02-18 13:31:27 -0800

    Add files via upload

---
+++ Amazon-Product_Market_Share/Amazon_Shopify_strata.sql
@@ -0,0 +1,10 @@
+with cte1 as(select territory_id,prod_brand, extract(quarter from order_date) as quart,extract(year from order_date) as yr from fct_customer_sales as fcs
+inner join map_customer_territory as mct
+on fcs.cust_id=mct.cust_id
+inner join dim_product dp
+on fcs.prod_sku_id=dp.prod_sku_id),
+
+cte2 as (select territory_id,prod_brand,count(prod_brand) over(partition by territory_id,prod_brand) as cnt1, count(prod_brand) over(partition by territory_id) as cnt2 from cte1 where quart=04 and yr=2021)
+
+select distinct territory_id, prod_brand, (cnt1/cnt2)*100 as Market_Share from cte2 where (cnt1/cnt2)*100!=0 ;
+#Prayas Dixit
